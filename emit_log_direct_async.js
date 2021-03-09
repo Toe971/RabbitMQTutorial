01074da36415adac2bfacd0b2e8a26c219a7b510
toe971 <liew179@gmail.com>
2021-03-09 10:17:23 +0800

    edited receive_logs

---
+++ emit_log_direct_async.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+const amqp = require('amqplib');
+const { Buffer } = require('buffer');
+
+//$ Variables
+
+const exchange = 'direct_logs'
+const args = process.argv.slice(2)
+console.log(args)
+const msg = args.slice(1).join(' ') || 'Hello World'
+
+// connect to RabbitMQ server
+const main = async () => {
+    const conn = await amqp.connect('amqp://localhost')
+    const channel = await conn.createChannel()
+    const severity = (args.length > 0) ? args[0] : 'info'
+
+    // declare exchange, publishing to a non-exisiting exchange is forbidden
+    await channel.assertExchange(exchange, 'direct', {
+        durable: false
+    })
+
+    
+    channel.publish(exchange, severity, Buffer.from(msg))
+
+    console.log(` [x] Sent ${severity}: ${msg}`)
+
+    setTimeout(() => {
+        conn.close()
+        process.exit(0)
+    }, 500)
+}
+
+main().catch(console.warn);
+
+  
